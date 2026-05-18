c197e9082a9874fdef6d48fcecd43bbb6ed52722
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2026-05-18 20:02:08 +0600

    auth controller updated

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('/register')
+    async register(@Body() dto: any) {
+        return this.authService.register(dto);
+    }
+
+    @Post('/login')
+    async login(@Body() dto: any) {
+        return this.authService.login(dto);
+    }
+}
