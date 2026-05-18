ac870bc62d0299492aed14c61e5527f2c2b14c00
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2026-05-18 19:54:43 +0600

    Auth service updated

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable prettier/prettier */
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private userService: UserService,
+        private jwtService: JwtService,
+    ){}
+
+    async register(dto: any) {
+        const existUser = await this.userService.findByEmail(dto.email);
+
+        if(existUser){
+            throw new Error('User already exist');
+        }
+        
+        const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+        const user = await this.userService.create({
+            name: dto.name,
+            email: dto.email,
+            password: hashedPassword
+        })
+
+        return this.generateToken(user);
+    }
+
+    async login(dto: any) {
+        const user = await this.userService.findByEmail(dto.email);
+
+        if(!user){
+            throw new UnauthorizedException('User not found');
+        }
+
+        const isPasswordMatch = await bcrypt.compare(dto.password, user.password);
+
+        if(!isPasswordMatch){
+            throw new UnauthorizedException('Invalid password');
+        }
+
+        return this.generateToken(user);
+    }
+
+    generateToken(user: any) {
+        const payload = {
+            sub: user.id, 
+            email: user.email,
+            role: user.role
+        };
+        return {
+            access_token: this.jwtService.sign(payload)
+        }
+    }
+
+
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,55 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private prisma: PrismaService) {}
+
+    findAll(){
+        return this.prisma.user.findMany({
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                role: true
+            }
+        });
+    }
+
+    findById(id: string){
+        return this.prisma.user.findUnique({
+            where: {id},
+        })
+    }
+
+    findByEmail(email: string){
+        return this.prisma.user.findUnique({
+            where: {email},
+        })
+    }
+
+    create(data: any){
+        return this.prisma.user.create({
+            data,
+        })
+    }
+
+    update(id: string, data: any){
+        return this.prisma.user.update({
+            where: {id},
+            data,
+        })
+    }
+
+    delete(id: string){
+        return this.prisma.user.delete({
+            where: {id},
+        })
+    }
+
+}
